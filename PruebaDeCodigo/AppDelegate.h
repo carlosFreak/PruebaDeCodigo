72328df63451f547a8c846afa75e2965e210843a
Carlos jimenez <carlosjimenez@iMac-de-Carlos.local>
2015-07-29 10:55:00 +0200

    Initial Commit

---
+++ PruebaDeCodigo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PruebaDeCodigo
+//
+//  Created by Carlos jimenez on 29/07/15.
+//  Copyright (c) 2015 Carlos jimenez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PruebaDeCodigo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PruebaDeCodigo
+//
+//  Created by Carlos jimenez on 29/07/15.
+//  Copyright (c) 2015 Carlos jimenez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
